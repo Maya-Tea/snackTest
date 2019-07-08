eac1d22c64771cd67c8ab3d8d97ca137ae8c8f30
Mayat Smolnik <mcsmolnik@gmail.com>
2019-07-08 14:59:08 -0500

    personal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -32,7 +32,7 @@ export default function HomeScreen() {
         <View style={styles.getStartedContainer}>
           <DevelopmentModeNotice />
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
+          <Text style={styles.getStartedText}>MAYAT LOVES SNAKITY SNACKS!!</Text>
 
           <View
             style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
